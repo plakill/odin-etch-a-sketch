b16834391ba82f2fcd46a6914d43811f8071fd3c
plakill <plakillexists@gmail.com>
2023-07-09 15:39:11 +0200

    Make grid resizeable with a button

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 let container = document.querySelector(".container");
-let gridSize = 16; // 16 means 16x16 
-for (let i = 0; i < gridSize*gridSize; i++) {
-    let div = document.createElement("div");
-    div.className = "pixel";
-    container.appendChild(div)
+let gridSize = 8; // 16 means 16x16
+let containerSize = 320; // 320 means 320px x 320px
 
-    div.addEventListener("mouseover", () => {
-        div.classList.toggle("painted");;
+resizeButton = document.querySelector("button.resize");
+
+resizeButton.addEventListener("click", () => {
+    if (gridSize != 64) {
+        gridSize *= 2;
+    } else {
+        gridSize = 8;
+    }
+    resizeButton.textContent = `${gridSize} x ${gridSize}`;
+    makeGrid();
+});
+
+const makeGrid = () => {
+    let pixelSize = containerSize / gridSize;
+    container.style.height = "fit-content";
+    container.style.height = containerSize + gridSize*2 + "px"; // gridSize*2 accounts for borders 
+    container.style.width = "fit-content";
+    container.style.width = containerSize + gridSize*2 + "px"; 
+
+    let oldPixels = document.querySelectorAll(".pixel");
+    oldPixels.forEach((pixel) => {
+        pixel.remove();
     });
-}
+
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        let pixel = document.createElement("div");
+        pixel.className = "pixel";
+        pixel.style.width = pixelSize + "px";
+        pixel.style.height = pixelSize + "px";
+        container.appendChild(pixel)
+    
+        pixel.addEventListener("mouseover", () => {
+            pixel.classList.toggle("painted");;
+        });
+    };
+};
+
+makeGrid();
 
